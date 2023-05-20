0156031499c69ef84ddfb96be37e5c8884430573
leobennett85 <103042120+leobennett85@users.noreply.github.com>
2023-05-20 13:46:20 -0230

    Added Dazzle Component

---
+++ MMSalonV2/src/components/Dazzle/Dazzle.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
+const Dazzle = () => {
+
+{/* Styled Components */}
+const Speck = styled(motion.div)`
+    width: 2px;
+    height: 2px;
+    display: flex;
+    position: absolute;
+    background-color: red;
+`;
+
+    <>
+        <Speck />
+    </>
+}
+
+export default Dazzle;
