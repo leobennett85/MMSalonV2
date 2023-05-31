37f0b41ffb7c6c32aaf82e75bbedfbd3633d814c
leobennett85 <leobennett85@mail.com>
2023-05-31 20:03:42 -0230

    Testing dazzle loop

--- MMSalonV2/src/components/Dazzle/Dazzle.jsx
+++ MMSalonV2/src/components/Dazzle/Dazzle.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { motion } from "framer-motion";
+import { animate, motion } from "framer-motion";
 
 {/* TODO
     - Create function to randomize initiated dimesions of dazzle obejcts - DONE
@@ -11,7 +11,6 @@ import { motion } from "framer-motion";
 
 
 const Dazzle = () => {
-    {/* Style Variables and Functions */}
 
     //#region Dimensions
     const dimension1 = "10px";
@@ -19,14 +18,15 @@ const Dazzle = () => {
     const dimension3 = "6px";
     const dimension4 = "4px";
     const dimension5 = "2px";
-    {/* create an array of possiblie Dimensions */}
+    // create an array of possiblie Dimensions */}
     const possibleDimensions = [dimension1, dimension2, dimension3, dimension4, dimension5];
-    {/* dimensionPicker function to choose random index of array */}
+    // dimensionPicker function to choose random index of array
     function DimensionPicker() {
         const randomIndex = Math.floor(Math.random() * possibleDimensions.length);
         return randomIndex;
     }
-    {/* dimensionPicker test loop */}
+    // dimensionPicker test loop
+    {/*
     function TestPicker() {
         let i = 0;
         let dimension = '';
@@ -37,38 +37,40 @@ const Dazzle = () => {
           } while (i < 15);
     }
     console.log(TestPicker());
+    */}
     //#endregion Dimensions
 
     //#region colors
-    const fleckColor1 = "#ffdddd";
-    const fleckColor2 = "#ddffff";
-    const fleckColor3 = "#c5d9ff";
-    const fleckColor4 = "#fcffdf";
-    const fleckColor5 = "#ddffe0";
-    {/* create an array of possible colors */}
-    const possibleColors = [fleckColor1, fleckColor2, fleckColor3, fleckColor4, fleckColor5];
-    {/* dimensionPicker function to choose random index of array */}
+    const speckColor1 = "#ffdddd";
+    const speckColor2 = "#ddffff";
+    const speckColor3 = "#c5d9ff";
+    const speckColor4 = "#fcffdf";
+    const speckColor5 = "#ddffe0";
+    // create an array of possible colors
+    const possibleColors = [speckColor1, speckColor2, speckColor3, speckColor4, speckColor5];
+    // dimensionPicker function to choose random index of array
     function ColorPicker() {
         const randomIndex = Math.floor(Math.random() * possibleColors.length);
         return randomIndex;
     }
-    {/* colorPicker test loop */}
+    {/*
+    // colorPicker test loop
     function TestColorPicker() {
         let i = 0;
         let colorPicked = '';
         do {
-            colorPicked = ColorPicker();
-            console.log(i + ": " + colorPicked);
-            console.log("Color: " + possibleColors[colorPicked]);
-            i = i + 1;
+                colorPicked = ColorPicker();
+                console.log(i + ": " + colorPicked);
+                console.log("Color: " + possibleColors[colorPicked]);
+                i = i + 1;
             } while (i < 15);
-    }
+        }
     console.log(TestColorPicker());
+    */}
     //#endregion Colors
 
-    {/* End of Style Variables and Functions */}
-
-    {/* Styled Components */}
+    //#Region Style Variables and Functions
+    //Styled Components
     const FullSiteContainer = styled.div`
         width: 100vw;
         height: 100vh;
@@ -77,47 +79,36 @@ const Dazzle = () => {
         top: 0;
         left: 0;
     `;
+    // Testing points for Speck
+    const point1x = "100vw";
+    const point1y = "100vh";
 
     const Speck = styled(motion.div)`
         width: ${possibleDimensions[DimensionPicker()]};
         height: ${possibleDimensions[DimensionPicker()]};
         background-image: linear-gradient(to right, ${possibleColors[ColorPicker()]}, ${possibleColors[ColorPicker()]}, ${possibleColors[ColorPicker()]} );
-        border: 1px solid black;
-    `;
-    {/* End of Styled Components */}
+        // border: 1px solid black;
+    `;  
 
-    {/*Testing points */}
-    const point1x = "calc(100vw - 10vw)";
-    const point1y = "calc(100vh - 80vh)";
-
-    {/*Testing recreation*/}
-    const addSpeck = `document.createElement(Speck)`;
-    function Recreate() {
-        let i = 0;
-        do {
-            addSpeck;
-            i = i + 1;
-          } while (i < 15);
-    }
+    //#endregion Style Variables and Functions
     
     return (
         <>
         <FullSiteContainer>
-            <Recreate>         
-            <Speck
-                animate = {{
-                    rotateX: 180,
-                    rotateY: 180,
-                    x: `${point1x}`,
-                    y: `${point1y}`,
-                }}
-                transition={{
-                    repeat: Infinity,
-                    ease: [0.25, 0.50, 0.75, 1],
-                    duration: 10
-                }}>
-            </Speck>
-            </Recreate>
+                <Speck
+                    animate = {{
+                        rotateX: 180,
+                        rotateY: 180,
+                        x: `${point1x}`,
+                        y: `${point1y}`,
+                    }}
+                    transition={{
+                        repeat: Infinity,
+                        ease: [0.25, 0.50, 0.75, 1],
+                        duration: 10,
+
+                        }}>
+                    </Speck>
             </FullSiteContainer>
         </>
     )
